241f153d59a3e368ae3a0e54d00b0210a216b49a
anuveyatsu <anuar.ustayev@gmail.com>
2017-11-23 15:31:45 +0600

    [login][s]: implemented login module using new datahub library - refs #7
    TODO: datahub library is not yet published to NPM

---
+++ main/utils/login.js
@@ -0,0 +1,31 @@
+const {config} = require('datahub')
+const {login, authenticate} = require('datahub')
+
+const { error: showError } = require('../dialogs')
+
+
+module.exports = async () => {
+  const apiUrl = config.get('api') || 'https://api.datahub.io'
+  const token = config.get('token')
+
+  let out
+  try {
+    out = await authenticate(apiUrl, token)
+  } catch (err) {
+    showError(err)
+    return
+  }
+
+  if (out.authenticated) {
+    showError('You are already logged in.')
+    return
+  }
+
+  const authUrl = out.providers.github.url
+  try {
+    await login(apiUrl, authUrl)
+  } catch (err) {
+    showError(err)
+    return
+  }
+}
